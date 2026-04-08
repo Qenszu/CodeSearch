292b4589d339c3220ca943e0d158e519f7307f67
kajetanserwecinski <kajetan.serwecinski@gmail.com>
2026-04-08 16:30:23 +0200

    Make DB OOP

--- code/demo.py
+++ code/demo.py
@@ -1,53 +1,23 @@
-from sentence_transformers import SentenceTransformer, util
-import torch
-from parser_ast import parser
+from pprint import pprint
 
-#Flax demo
+from db import DB
+from parser_ast import search
+from pathlib import Path
 
-model = SentenceTransformer('flax-sentence-embeddings/st-codesearch-distilroberta-base')
+DB = DB("Test")
+arr = search("/Users/kajetanserwecinski/Documents/Programming/CodeSearchQuenszu/code")
 
-code_snippets = [
-"def save_user_profile(user_id, data): return db.users.update(user_id, data)",
-    "def validate_token(token): return jwt.decode(token, SECRET_KEY, algorithms=['HS256'])",
-    "def sunny(city): return requests.get(f'https://api.weather.com/{city1}')",
-    "def rainy(city): return requests.get(f'https://api.weather.com/{city2}')",
-    "class connect: def connect(self): self.conn = psycopg2.connect(DB_URL)",
-    "def warta(graph): stack=[] while len(stack)!=0: p = stack.pop() for nei in p",
-    "def quick_sort(graph): if len(arr) <= 1: return arr pivot = arr[len(arr) // 2] left = [x for x in arr if x < pivot] middle = [x for x in arr if x == pivot] right = [x for x in arr if x > pivot] return quicksort(left) + middle + quicksort(right)"
-    "def buble_sort(a): for i in range(len(a)): for j in range(len(a)-i-1): if a[j] > a[j+1]: a[j], a[j+1] = a[j+1], a[j]"
-]
+n = len(arr)
 
-my_code = parser("ast_tests.py")
+ids = [str(i) for i in range(1, n+1)]
 
-code_embeddings = model.encode(my_code, convert_to_tensor=True)
+DB.update(arr, ids)
 
+while True:
+    print("Heare I am")
+    s = input()
 
-def intelligent_search(query):
-    query_embedding = model.encode(query, convert_to_tensor=True)
-    cosine_scores = util.cos_sim(query_embedding, code_embeddings)[0]
-    best_match_idx = torch.argmax(cosine_scores).item()
+    if s == 'q':
+        break
 
-    return my_code[best_match_idx], cosine_scores[best_match_idx].item()
-
-questions = [
-    "Validate user",
-    "Connected to database",
-    "Load weather",
-    "DatabaseConnector",
-    "Nice weather",
-    "Bad weather",
-    "Bfs",
-    "sorting",
-    "2 fors loops"
-]
-
-my_questions = [
-    "find add fun", 
-    "hello"] 
-
-print("==========================\n")
-for q in my_questions:
-    result, score = intelligent_search(q)
-    print(f"Pytanie: '{q}'")
-    print(f"Znaleziony kod: {result}")
-    print(f"Pewność (Score): {score:.4f}\n")
+    pprint(DB.query(s, 3))
