34ca5bc39bf889b22e546e61edd09ac5bf875133
kajetanserwecinski <kajetan.serwecinski@gmail.com>
2026-04-08 15:57:23 +0200

    Make DB OOP

--- code/db.py
+++ code/db.py
@@ -1,23 +1,22 @@
 import chromadb
 from model import MyEmbeddingFunc
+from typing import Any
 
-F = MyEmbeddingFunc()
-
-chroma_client = chromadb.Client()
-
-collection = chroma_client.create_collection(
-    name="code",
-    embedding_function=F
-)
-
-collection.add(
-    documents=["def hello_world(): print('hi')", "class Database: pass"],
-    ids=["id1", "id2"]
-)
-
-results = collection.query(
-    query_texts=["Print Hi"],
-    n_results=1
-)
-
-print(results)
+class DB:
+    def __init__(self, name: str):
+        func = MyEmbeddingFunc()
+        chroma_client = chromadb.Client()
+        self.collection = (chroma_client.create_collection(
+            name=name,
+            embedding_function=func
+        ))
+    def update(self, doc: list[str], id_list: list[str]) -> None:
+        self.collection.add(
+            documents=doc,
+            ids=id_list
+        )
+    def query(self, text, num_of_result) -> dict[str, Any]:
+        return self.collection.query(
+            query_texts=text,
+            n_results=num_of_result
+        )
