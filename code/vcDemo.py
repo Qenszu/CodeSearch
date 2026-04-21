0e564dfee76567a5f6b4f8a034a9b2b39130ffb9
Qenszu <Opalusaktv@gmail.com>
2026-04-21 14:47:19 +0200

    add voice command function

--- code/vcDemo.py
+++ code/vcDemo.py
@@ -3,6 +3,7 @@ from pprint import pprint
 from db import DB
 from parser_ast import search
 from pathlib import Path
+from voiceRecognision import voice
 
 DB = DB("Test")
 arr = search("C:/Users/user/Documents/github/CodeSearch/code")
@@ -12,12 +13,5 @@ n = len(arr)
 ids = [str(i) for i in range(1, n+1)]
 
 DB.update(arr, ids)
+voice(DB)
 
-while True:
-    print("Heare I am")
-    s = input()
-
-    if s == 'q':
-        break
-
-    pprint(DB.query(s, 3))
---
+++ code/voiceRecognision.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr
+from voiceRecognitionFun import activeMicrophone
+from db import DB
+from pprint import pprint
+
+def voice(DB):
+    r = sr.Recognizer()
+
+    print("Initializing Speech Recognition...")
+
+    active_microphone_list = activeMicrophone.get_active_microphones()
+    print(f"Choose your device input:")
+    for i, src in enumerate(active_microphone_list):
+        print(f"Microphone {i}: {src}")
+
+    ind = int(input("Select your microphone: "))
+
+    if ind < 0 or ind >= len(active_microphone_list):
+        raise ValueError("Invalid input")
+
+    print("1. Polski\n2. English")
+    lan = int(input("Select your language: "))
+
+    match lan:
+        case 1:
+            lan = "pl-PL"
+        case 2:
+            lan = "en-EN"
+        case _:
+            lan = "pl-PL"
+
+    while True:
+        print("Speek now")
+        with sr.Microphone(device_index=ind) as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+
+            try:
+                text = r.recognize_google(audio, language=lan)
+                print(text)
+                if text == "exit" or text == "koniec":
+                    return
+                pprint(DB.query(text, 3))
+
+
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+
+
