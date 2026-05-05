cb3c3ad9c5a11edbc1351d15674f6ec3d18c100f
kajetanserwecinski <kajetan.serwecinski@gmail.com>
2026-05-05 14:00:25 +0200

    Adding smart updates to CLI

--- code/cli.py
+++ code/cli.py
@@ -1,5 +1,6 @@
 import argparse
 
+from smart_update import smart_update
 from db import DB
 
 from pretty_print import pretty_print
@@ -11,6 +12,7 @@ def main():
 
     parser.add_argument('-i', '--info', action='store_true', help='Wyświetla informacje')
     parser.add_argument('-f', '--file', action='store_true', help='Przyjmuje nazwę pliku')
+    parser.add_argument('-u', '--update', action='store_true', help='Uaktualnie bazy danych')
 
     args = parser.parse_args()
 
@@ -31,6 +33,13 @@ def main():
 
             result = db.query([query], 3)
             pretty_print(result)
+
+    if args.update:
+        changes = smart_update('HEAD~1', 'HEAD')
+
+        print("-" * 30)
+        for c in changes:
+            print(f"[{c['status']}] {c['path']}")
 
     if not (args.info or args.file):
         parser.print_help()
